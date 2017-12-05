4eb50593cb1819c5536779517504fdbd40269f0e
Kavec1 <matus.kavec@gmail.com>
2017-12-05 20:43:54 +0100

    Update and rename prišerky.py to priserky.py

---
+++ priserky.py
@@ -0,0 +1,40 @@
+import tkinter
+c=tkinter.Canvas()
+c.pack()
+
+x=100
+y=100
+
+def priserka1(x,y):
+  c.create_rectangle(x-10,y-20,x+10,y-15, fill='blue', outline='blue')
+  c.create_rectangle(x-20,y-15,x+20,y-10, fill='blue', outline='blue')
+  c.create_rectangle(x-25,y-10,x+25,y+5, fill='blue', outline='blue')
+  c.create_rectangle(x-20,y-5,x-10,y,fill='black', outline='blue')
+  c.create_rectangle(x+10,y-5,x+20,y, fill='black', outline='blue')
+  c.create_rectangle(x-15,y+5,x-10,y+10, fill='blue', outline='blue')
+  c.create_rectangle(x+10,y+5,x+15,y+10, fill='blue', outline='blue')
+  c.create_rectangle(x-20,y+10,x-15,y+15, fill='blue', outline='blue')
+  c.create_rectangle(x-10,y+10,x+10,y+15, fill='blue', outline='blue')
+  c.create_rectangle(x+15,y+10,x+20,y+15, fill='blue', outline='blue')
+  c.create_rectangle(x-25,y+15,x-20,y+20, fill='blue', outline='blue')
+  c.create_rectangle(x+20,y+15,x+25,y+20, fill='blue', outline='blue')
+
+x=200
+y=100
+
+def priserka2(x,y):
+  c.create_rectangle(x-5,y-20,x+5,y-15, fill='green', outline='green')
+  c.create_rectangle(x-10,y-15,x+10,y-10, fill='green', outline='green')
+  c.create_rectangle(x-15,y-10,x+15,y-5, fill='green', outline='green')
+  c.create_rectangle(x-20,y-5,x+20,y+5, fill='green', outline='green')
+  c.create_rectangle(x-10,y-5,x-5,y, fill='black', outline='green')
+  c.create_rectangle(x+5,y-5,x+10,y, fill='black', outline='green')
+  c.create_rectangle(x-10,y+5,x-5,y+10, fill='green', outline='green')
+  c.create_rectangle(x+5,y+5,x+10,y+10, fill='green', outline='green')
+  c.create_rectangle(x-15,y+10,x-10,y+15, fill='green', outline='green')
+  c.create_rectangle(x-5,y+10,x+5,y+15, fill='green', outline='green')
+  c.create_rectangle(x+10,y+10,x+15,y+15, fill='green', outline='green')
+  c.create_rectangle(x-20,y+15,x-15,y+20, fill='green', outline='green')
+  c.create_rectangle(x-10,y+15,x-5,y+20, fill='green', outline='green')
+  c.create_rectangle(x+5,y+15,x+10,y+20, fill='green', outline='green')
+  c.create_rectangle(x+15,y+15,x+20,y+20, fill='green', outline='green')
