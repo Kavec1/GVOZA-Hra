e237a3e31f8f85c3e0a2a46e472c02c2fadb97b2
Kavec1 <matus.kavec@gmail.com>
2017-12-08 06:46:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from init import *
 from objects import *
+from move import *
+#from threading import *
 
 priserka1(100,100)
 priserka2(200,200)
+c.bind_all('<Up>', hore)
+c.bind_all('<Down>', dole)
+c.bind_all('<Left>', lava)
+c.bind_all('<Right>', prava)
+
