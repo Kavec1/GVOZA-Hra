cf4767360d01271a2aaf405e0465e5c9634da000
Kavec1 <matus.kavec@gmail.com>
2017-12-08 06:48:08 +0100

    Delete timer.py

--- timer.py
+++
@@ -1,9 +0,0 @@
-def create(obj):
-  if obj==1:
-    priserka1(100,100)
-  elif obj==2:
-    priserka2(200,200)
-    
-t = Timer(30 , create) #timer ktory vykonava funkciu create kazdých 30 sekund
-t.start() #spustenie timera
-t.cancel() #ukonceine timera
