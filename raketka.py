d207754c1889d8c55dfd2769a0f2ca67aeca28cb
Kavec1 <matus.kavec@gmail.com>
2017-12-10 20:06:09 +0100

    Delete raketka.py

--- raketka.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-c=tkinter.Canvas()
-c.pack()
-x=y=100
-
-##raketka
-c.create_rectangle(x,y-20,x+10,y-10,fill='white', outline='white')
-c.create_rectangle(x-20,y-10,x+30,y+10,fill='white', outline='white')
-c.create_rectangle(x-30,y+10,x+40,y+20,fill='white', outline='white')
