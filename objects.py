43835f1471c71f6096909321426d46345a381eab
Kavec1 <matus.kavec@gmail.com>
2017-12-10 19:56:47 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -31,10 +31,10 @@ def priserka2(x,y):
   c.create_rectangle(x+15,y+15,x+20,y+20, fill='green', outline='green')
   
   def raketka(x,y):
-  c.create_rectangle(x,y-20,x+10,y-10,fill='white', outline='white')
-  c.create_rectangle(x-20,y-10,x+30,y+10,fill='white', outline='white')
-  c.create_rectangle(x-30,y+10,x+40,y+20,fill='white', outline='white')
-  c.create_oval(10,10,50,50, tags = 'ujco')
+  c.create_rectangle(x,y-20,x+10,y-10,fill='white', outline='white', tags='raketa')
+  c.create_rectangle(x-20,y-10,x+30,y+10,fill='white', outline='white', tags='raketa')
+  c.create_rectangle(x-30,y+10,x+40,y+20,fill='white', outline='white', tags='raketa')
+'''c.create_oval(10,10,50,50, tags = 'ujco')
 c.create_oval(20,20,25,25, tags = 'ujco')
 c.create_oval(35,20,40,25, tags = 'ujco')
-c.create_line(20,40,40,40, tags = 'ujco')
+c.create_line(20,40,40,40, tags = 'ujco')'''
