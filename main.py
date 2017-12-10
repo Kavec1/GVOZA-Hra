4e0e44cddc469e4545158b4cc9637ae9613e4b40
Kavec1 <matus.kavec@gmail.com>
2017-12-10 19:43:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,4 @@ from move import *
 
 priserka1(100,100)
 priserka2(200,200)
-c.bind_all('<Up>', hore)
-c.bind_all('<Down>', dole)
-c.bind_all('<Left>', lava)
-c.bind_all('<Right>', prava)
-
+c.bind('<Motion>', move)
