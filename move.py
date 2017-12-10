fba4b02323101d983bb3b4a47df91d653eb9e9c4
Kavec1 <matus.kavec@gmail.com>
2017-12-10 19:55:16 +0100

    Update move.py

--- move.py
+++ move.py
@@ -6,6 +6,6 @@ def move(udalost):
     y[0]=udalost.y
     print(str(x),str(y))    
     print(str(x[1]-x[0]),str(y[1]-y[0]))
-    c.move('ujco', -(x[1]-x[0]), -(y[1]-y[0]))
+    c.move('raketa', -(x[1]-x[0]), -(y[1]-y[0]))
     x[1]=x[0]
     y[1]=y[0]
