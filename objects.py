f17f0351b7cece7f114cff313b52ad8b927a94e4
Kavec1 <matus.kavec@gmail.com>
2017-12-11 17:46:49 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,40 +1,40 @@
 from init import *
 
 #objekt priserka1
-def priserka1(x,y):
-  c.create_rectangle(x-10,y-20,x+10,y-15, fill='blue', outline='blue')
-  c.create_rectangle(x-20,y-15,x+20,y-10, fill='blue', outline='blue')
-  c.create_rectangle(x-25,y-10,x+25,y+5, fill='blue', outline='blue')
-  c.create_rectangle(x-20,y-5,x-10,y,fill='black', outline='blue')
-  c.create_rectangle(x+10,y-5,x+20,y, fill='black', outline='blue')
-  c.create_rectangle(x-15,y+5,x-10,y+10, fill='blue', outline='blue')
-  c.create_rectangle(x+10,y+5,x+15,y+10, fill='blue', outline='blue')
-  c.create_rectangle(x-20,y+10,x-15,y+15, fill='blue', outline='blue')
-  c.create_rectangle(x-10,y+10,x+10,y+15, fill='blue', outline='blue')
-  c.create_rectangle(x+15,y+10,x+20,y+15, fill='blue', outline='blue')
-  c.create_rectangle(x-25,y+15,x-20,y+20, fill='blue', outline='blue')
-  c.create_rectangle(x+20,y+15,x+25,y+20, fill='blue', outline='blue')
+def priserka1(x,y,tag):
+  c.create_rectangle(x-10,y-20,x+10,y-15, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x-20,y-15,x+20,y-10, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x-25,y-10,x+25,y+5, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x-20,y-5,x-10,y,fill='black', outline='blue', tags=tag)
+  c.create_rectangle(x+10,y-5,x+20,y, fill='black', outline='blue', tags=tag)
+  c.create_rectangle(x-15,y+5,x-10,y+10, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x+10,y+5,x+15,y+10, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x-20,y+10,x-15,y+15, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x-10,y+10,x+10,y+15, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x+15,y+10,x+20,y+15, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x-25,y+15,x-20,y+20, fill='blue', outline='blue', tags=tag)
+  c.create_rectangle(x+20,y+15,x+25,y+20, fill='blue', outline='blue', tags=tag)
   
 #objekt priserka2  
-def priserka2(x,y):
-  c.create_rectangle(x-5,y-20,x+5,y-15, fill='green', outline='green')
-  c.create_rectangle(x-10,y-15,x+10,y-10, fill='green', outline='green')
-  c.create_rectangle(x-15,y-10,x+15,y-5, fill='green', outline='green')
-  c.create_rectangle(x-20,y-5,x+20,y+5, fill='green', outline='green')
-  c.create_rectangle(x-10,y-5,x-5,y, fill='black', outline='green')
-  c.create_rectangle(x+5,y-5,x+10,y, fill='black', outline='green')
-  c.create_rectangle(x-10,y+5,x-5,y+10, fill='green', outline='green')
-  c.create_rectangle(x+5,y+5,x+10,y+10, fill='green', outline='green')
-  c.create_rectangle(x-15,y+10,x-10,y+15, fill='green', outline='green')
-  c.create_rectangle(x-5,y+10,x+5,y+15, fill='green', outline='green')
-  c.create_rectangle(x+10,y+10,x+15,y+15, fill='green', outline='green')
-  c.create_rectangle(x-20,y+15,x-15,y+20, fill='green', outline='green')
-  c.create_rectangle(x-10,y+15,x-5,y+20, fill='green', outline='green')
-  c.create_rectangle(x+5,y+15,x+10,y+20, fill='green', outline='green')
-  c.create_rectangle(x+15,y+15,x+20,y+20, fill='green', outline='green')
+def priserka2(x,y,tag):
+  c.create_rectangle(x-5,y-20,x+5,y-15, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x-10,y-15,x+10,y-10, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x-15,y-10,x+15,y-5, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x-20,y-5,x+20,y+5, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x-10,y-5,x-5,y, fill='black', outline='green', tags=tag)
+  c.create_rectangle(x+5,y-5,x+10,y, fill='black', outline='green', tags=tag)
+  c.create_rectangle(x-10,y+5,x-5,y+10, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x+5,y+5,x+10,y+10, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x-15,y+10,x-10,y+15, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x-5,y+10,x+5,y+15, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x+10,y+10,x+15,y+15, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x-20,y+15,x-15,y+20, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x-10,y+15,x-5,y+20, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x+5,y+15,x+10,y+20, fill='green', outline='green', tags=tag)
+  c.create_rectangle(x+15,y+15,x+20,y+20, fill='green', outline='green', tags=tag)
 
 #objekt priserka3
-def priserka3(x,y):
+def priserka3(x,y,tag):
   pass
 
 #objekt raketa
