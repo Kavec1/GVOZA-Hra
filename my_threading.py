a907a57f51d8c2e577dd1059b278ec3ef1fcf6eb
Kavec1 <matus.kavec@gmail.com>
2017-12-11 16:26:23 +0100

    Update my_threading.py

--- my_threading.py
+++ my_threading.py
@@ -2,4 +2,25 @@ import threading
 from move import *
 def pohyb_raketa():
     c.bind('<Motion>', move_raketa)
+def make_p1():
+    while(1):
+        priserka1(randint(0,800),randint(0,1000))
+        print('priserka1 zije')
+        sleep(5)
+
+def make_p2():
+    while(1):
+        priserka2(randint(0,800),randint(0,1000))
+        print('priserka2 zije')
+        sleep(7.5)
+
+def make_p3():
+    while(1):
+        priserka3(randint(0,800),randint(0,1000))
+        print('priserka3 zije')
+        sleep(10)
+        
 pohyb = threading.Thread(target=pohyb_raketa)
+thread_p1 = threading.Thread(target=make_p1)
+thread_p2 = threading.Thread(target=make_p2)
+thread_31 = threading.Thread(target=make_p3)
