d58aedf2bb472fb49b1afca1ece6d7023a07f088
Kavec1 <matus.kavec@gmail.com>
2017-12-11 17:38:47 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,4 +1,6 @@
 from init import *
+
+#objekt priserka1
 def priserka1(x,y):
   c.create_rectangle(x-10,y-20,x+10,y-15, fill='blue', outline='blue')
   c.create_rectangle(x-20,y-15,x+20,y-10, fill='blue', outline='blue')
@@ -13,6 +15,7 @@ def priserka1(x,y):
   c.create_rectangle(x-25,y+15,x-20,y+20, fill='blue', outline='blue')
   c.create_rectangle(x+20,y+15,x+25,y+20, fill='blue', outline='blue')
   
+#objekt priserka2  
 def priserka2(x,y):
   c.create_rectangle(x-5,y-20,x+5,y-15, fill='green', outline='green')
   c.create_rectangle(x-10,y-15,x+10,y-10, fill='green', outline='green')
@@ -29,13 +32,13 @@ def priserka2(x,y):
   c.create_rectangle(x-10,y+15,x-5,y+20, fill='green', outline='green')
   c.create_rectangle(x+5,y+15,x+10,y+20, fill='green', outline='green')
   c.create_rectangle(x+15,y+15,x+20,y+20, fill='green', outline='green')
-  
+
+#objekt priserka3
+def priserka3(x,y):
+  pass
+
+#objekt raketa
 def raketka(x,y):
   c.create_rectangle(x,y-20,x+10,y-10,fill='white', outline='white', tags='raketa')
   c.create_rectangle(x-20,y-10,x+30,y+10,fill='white', outline='white', tags='raketa')
   c.create_rectangle(x-30,y+10,x+40,y+20,fill='white', outline='white', tags='raketa')
-  
-'''c.create_oval(10,10,50,50, tags = 'ujco')
-c.create_oval(20,20,25,25, tags = 'ujco')
-c.create_oval(35,20,40,25, tags = 'ujco')
-c.create_line(20,40,40,40, tags = 'ujco')'''
